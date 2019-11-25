0a1575204046a57e9fc63a6130e6b21b8af87b4d
zhouzhaoxin <singleorb@gmail.com>
2019-11-25 14:47:06 +0800

    simple html page

--- app/io/controller/game/drink.js
+++ app/io/controller/game/drink.js
@@ -2,19 +2,17 @@ const Controller = require('egg').Controller;
 
 class DrinkController extends Controller {
   async index() {
-    const { ctx, app } = this;
+    const {app} = this;
     const nsp = app.io.of('/drink');
-    const message = ctx.args[0] || {};
-    const socket = ctx.socket;
-    const client = socket.id;
-    try {
-      const { target, payload } = message;
-      if (!target) return;
-      const msg = ctx.helper.parseMsg('exchange', payload, { client, target });
-      nsp.emit(client, msg);
-    } catch (error) {
-      app.logger.error(error);
-    }
+    await nsp.adapter.clients(['demo'], async (err, clients) => {
+      let cli = clients[Math.floor(Math.random() * clients.length)];
+      try {
+        nsp.emit('start', cli);
+      } catch (error) {
+        app.logger.error(error);
+      }
+    });
+
   }
 }
 
